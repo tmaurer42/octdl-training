c188a3ad4799b039ed68860f16a3a5a267d093b3
Tim Maurer <s8timaur@stud.uni-saarland.de>
2024-08-19 00:48:22 +0200

    add test experiment for new fedbuff strategy

---
+++ mobilenet_fedbuff_test.py
@@ -0,0 +1,19 @@
+from federated_learning.optimization import main as run_experiment
+from shared.data import OCTDLClass
+
+
+def run_fedavg_experiments():
+    n_jobs = 1
+    classes = [OCTDLClass.AMD, OCTDLClass.NO]
+    n_clients = 20
+    buffer_size = 10
+    n_rounds = 40
+            
+    run_experiment(model_type='MobileNetV2', transfer_learning=True, loss_fn_type='WeightedCrossEntropy',
+                class_list=classes, optimization_mode='minimize_loss', n_jobs=n_jobs,
+                fl_strategy='FedBuff', n_clients=n_clients, n_rounds=n_rounds,
+                buffer_size=buffer_size)
+
+
+if __name__ == "__main__":
+    run_fedavg_experiments()

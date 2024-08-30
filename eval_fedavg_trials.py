b79b74d0db32da2ad96157d5bf536ff47ac2a767
Tim Maurer <s8timaur@stud.uni-saarland.de>
2024-08-30 14:22:44 +0200

    add fedavg eval

---
+++ eval_fedavg_trials.py
@@ -0,0 +1,39 @@
+from evaluate import FLEvalParameters, evaluate
+from shared.data import OCTDLClass
+
+
+def eval_fedbuff_experiments():
+    classes = [OCTDLClass.AMD, OCTDLClass.NO]
+    n_clients = 20
+
+    for buffer_size in [10, 5, 3]:
+        fl_eval_params = FLEvalParameters(
+            strategy='FedAvg', n_clients=n_clients, n_clients_per_round=buffer_size)
+        evaluate(
+            classes=classes,
+            model_type="ResNet18",
+            transfer_learning=True,
+            optimization_mode='maximize_f1_macro',
+            loss_fn_type='WeightedCrossEntropy',
+            fl_eval_parameters=fl_eval_params
+        )
+        evaluate(
+            classes=classes,
+            model_type="ResNet18",
+            transfer_learning=False,
+            optimization_mode='maximize_f1_macro',
+            loss_fn_type='WeightedCrossEntropy',
+            fl_eval_parameters=fl_eval_params
+        )
+        evaluate(
+            classes=classes,
+            model_type="MobileNetV2",
+            transfer_learning=True,
+            optimization_mode='maximize_f1_macro',
+            loss_fn_type='WeightedCrossEntropy',
+            fl_eval_parameters=fl_eval_params
+        )
+
+
+if __name__ == "__main__":
+    eval_fedbuff_experiments()

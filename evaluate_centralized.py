500ebfdebe5a6e83b5e04c07ef953973d7b75c07
Tim Maurer <s8timaur@stud.uni-saarland.de>
2024-09-02 17:04:41 +0200

    new centralized evaluation

--- evaluate_centralized.py
+++ evaluate_centralized.py
@@ -4,13 +4,13 @@ from evaluate import evaluate
 
 def main():
     evaluate([OCTDLClass.AMD, OCTDLClass.NO], 'ResNet18', transfer_learning=True,
-             optimization_mode='minimize_loss', loss_fn_type='CrossEntropy')
+             optimization_mode='maximize_f1_macro', loss_fn_type='WeightedCrossEntropy')
     evaluate([OCTDLClass.AMD, OCTDLClass.NO], 'ResNet18', transfer_learning=False,
-             optimization_mode='minimize_loss', loss_fn_type='CrossEntropy')
+             optimization_mode='maximize_f1_macro', loss_fn_type='WeightedCrossEntropy')
     evaluate([OCTDLClass.AMD, OCTDLClass.NO], 'MobileNetV2', transfer_learning=True,
-             optimization_mode='minimize_loss', loss_fn_type='CrossEntropy')
-    evaluate([OCTDLClass.AMD, OCTDLClass.NO], 'EfficientNetV2', transfer_learning=True,
-             optimization_mode='minimize_loss', loss_fn_type='CrossEntropy')
+             optimization_mode='maximize_f1_macro', loss_fn_type='WeightedCrossEntropy')
+    #evaluate([OCTDLClass.AMD, OCTDLClass.NO], 'EfficientNetV2', transfer_learning=True,
+    #         optimization_mode='minimize_loss', loss_fn_type='CrossEntropy')
 
 
 if __name__ == "__main__":

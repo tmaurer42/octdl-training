d180e19963e6c28f1fde3e7707b65281e385be8f
Tim Maurer <s8timaur@stud.uni-saarland.de>
2024-09-02 13:26:53 +0200

    update centralized experiments

--- run_all_centralized_experiments.py
+++ run_all_centralized_experiments.py
@@ -5,17 +5,17 @@ from shared.data import OCTDLClass
 def main():
     classes = [OCTDLClass.AMD, OCTDLClass.NO]
 
-    run_experiment(model_type='ResNet18', transfer_learning=True, loss_fn_type='CrossEntropy',
-                    class_list=classes, optimization_mode='minimize_loss', n_jobs=2)
+    run_experiment(model_type='ResNet18', transfer_learning=True, loss_fn_type='WeightedCrossEntropy',
+                    class_list=classes, optimization_mode='maximize_f1_macro', n_jobs=2)
 
-    run_experiment(model_type='ResNet18', transfer_learning=False, loss_fn_type='CrossEntropy',
-                    class_list=classes, optimization_mode='minimize_loss', n_jobs=2)
+    run_experiment(model_type='ResNet18', transfer_learning=False, loss_fn_type='WeightedCrossEntropy',
+                    class_list=classes, optimization_mode='maximize_f1_macro', n_jobs=2)
 
-    run_experiment(model_type='MobileNetV2', transfer_learning=True, loss_fn_type='CrossEntropy',
-                    class_list=classes, optimization_mode='minimize_loss', n_jobs=2)
+    run_experiment(model_type='MobileNetV2', transfer_learning=True, loss_fn_type='WeightedCrossEntropy',
+                    class_list=classes, optimization_mode='maximize_f1_macro', n_jobs=2)
                     
-    run_experiment(model_type='EfficientNetV2', transfer_learning=True, loss_fn_type='CrossEntropy',
-                    class_list=classes, optimization_mode='minimize_loss', n_jobs=2)
+    #run_experiment(model_type='EfficientNetV2', transfer_learning=True, loss_fn_type='WeightedCrossEntropy',
+    #                class_list=classes, optimization_mode='maximize_f1_macro', n_jobs=2)
 
 
 if __name__ == "__main__":
